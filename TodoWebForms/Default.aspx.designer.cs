45133f3345de56320a9fd21ff8dea048b409f9cf
Henry78119 <43590704+Henry78119@users.noreply.github.com>
2025-01-28 09:02:50 -0500

    Update designer file with new controls

--- TodoWebForms/Default.aspx.designer.cs
+++ TodoWebForms/Default.aspx.designer.cs
@@ -3,5 +3,8 @@ namespace TodoWebForms
     public partial class Default
     {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.TextBox txtNewTask;
+        protected global::System.Web.UI.WebControls.Button btnAddTask;
+        protected global::System.Web.UI.WebControls.GridView gvTasks;
     }
 }
